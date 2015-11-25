f8aa3e812984270ac3c6c8efa4d8c888c9152058
maehara08 <aso.riiiku08@gmail.com>
2015-11-26 01:11:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/riku/dragviewtestapp/DragViewListener.java
@@ -0,0 +1,45 @@
+package com.example.riku.dragviewtestapp;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by RIKU on 2015/11/26.
+ */
+public class DragViewListener implements View.OnTouchListener {
+
+    // ドラッグ対象のView
+    private TextView textView12;
+    // ドラッグ中に移動量を取得するための変数
+    private int oldx;
+    private int oldy;
+
+    public DragViewListener(TextView textView) {
+        this.textView12 = textView;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        // タッチしている位置取得
+        int x = (int) event.getRawX();
+        int y = (int) event.getRawY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                // 今回イベントでのView移動先の位置
+                int left = textView12.getLeft() + (x - oldx);
+                int top = textView12.getTop() + (y - oldy);
+                // Viewを移動する
+                textView12.layout(left, top, left + textView12.getWidth(), top
+                        + textView12.getHeight());
+                break;
+        }
+
+        // 今回のタッチ位置を保持
+        oldx = x;
+        oldy = y;
+        // falseにしないとほかのリスナーが呼ばれない
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/riku/dragviewtestapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.riku.dragviewtestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class MainActivity extends AppCompatActivity {
+    TextView dragView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        dragView = (TextView) findViewById(R.id.textView);
+        DragViewListener listener = new DragViewListener(dragView);
+        dragView.setOnTouchListener(listener);
+
+
+
+        dragView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showToast();
+
+
+                return false;//trueにすると他のリスナーが呼ばれない
+            }
+        });
+    }
+
+    private void showToast() {
+        Toast.makeText(this, "Toastです", Toast.LENGTH_SHORT).show();
+    }
+}
