23c14180b4eff917486833af7e4aeee131108958
maehara08 <aso.riiiku08@gamil.com>
2015-11-26 14:09:31 +0900

    Fragment Fixed

--- app/src/main/java/com/example/riku/dragviewtestapp/MainFragment.java
+++ app/src/main/java/com/example/riku/dragviewtestapp/MainFragment.java
@@ -1,8 +1,8 @@
 package com.example.riku.dragviewtestapp;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
