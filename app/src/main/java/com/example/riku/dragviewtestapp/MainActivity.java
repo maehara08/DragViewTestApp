adae6eaf14e334a2f69fee938798b0d90f74b787
maehara08 <aso.riiiku08@gamil.com>
2015-11-26 13:32:33 +0900

    Fragment

---
+++ app/src/main/java/com/example/riku/dragviewtestapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.riku.dragviewtestapp;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class MainActivity extends AppCompatActivity {
+    TextView dragView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        dragView = (TextView) findViewById(R.id.textView);
+        DragViewListener listener = new DragViewListener(dragView);
+        dragView.setOnTouchListener(listener);
+
+
+
+        dragView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showToast();
+
+
+                return false;//trueにすると他のリスナーが呼ばれない
+            }
+        });
+
+
+        //Fragmentを設置
+        // Fragmentを作成します
+        MainFragment fragment = new MainFragment();
+        // Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // 新しく追加を行うのでaddを使用します
+        // 他にも、メソッドにはreplace removeがあります
+        // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.add(R.id.container,fragment);
+        // 最後にcommitを使用することで変更を反映します
+        transaction.commit();
+    }
+//Toastのメソッド
+    private void showToast() {
+        Toast.makeText(this, "Toastです", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/riku/dragviewtestapp/MainFragment.java
@@ -0,0 +1,44 @@
+package com.example.riku.dragviewtestapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v4.app.FragmentTransaction;
+
+/**
+ * Created by riku_maehara on 2015/11/26.
+ */
+public class MainFragment extends Fragment {
+    TextView mTextView;
+
+    //Fragmentで表示するViewを作成するメソッド
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+    //Viewが生成し終わった後に呼ばれるメソッド
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        TextViewを紐付ける
+        mTextView=(TextView) view.findViewById(R.id.textView2);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText(mTextView.getText()+"!");
+            }
+        });
+
+
+    }
+}
